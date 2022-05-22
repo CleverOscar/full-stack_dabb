cadb1a8e7475596008e908deed9953b297a2bb3a
Oscar Pacheco <Oscarpa9188@gmail.com>
2022-05-22 13:57:02 -0500

    Test Complete

--- react-dabb/src/App.js
+++ react-dabb/src/App.js
@@ -1,27 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {ethers} from 'ethers'
 
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
 
 console.log(Greeter.abi)
 
 function App() {
+
+  const [greeting, setGreetingValue] = useState("");
+
+  async function requestAccount(){
+    await window.ethereum.request({method: 'eth_requestAccounts'});
+  }
+
+  async function fetchGreeting(){
+    if (typeof window.ethereum !== 'undefined'){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+
+      try {
+        const data = await contract.greet();
+        console.log('data', data)
+      } catch {
+        console.log('Error')
+      }
+
+    }
+  }
+
+  async function setGreeting(){
+    if(!greeting) return
+    if(typeof window.ethereum !== 'undefined'){
+      await requestAccount();
+      
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-center w-full">
+          Learn Web3 Development
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+        <div className='w-1/2 mx-auto flex flex-row justify-around mt-10'>
+          <button className="bg-blue-200 rounded-full p-2" onClick={fetchGreeting}>
+            Fetch Greeting
+          </button>
+
+          <button className="bg-blue-200 rounded-full p-2" onClick={setGreeting}>
+            Set Greeting
+          </button>
+          <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+        </div>
     </div>
   );
 }
